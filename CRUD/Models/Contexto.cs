6c74d5cd4ad5cf52418edf79a87068c64f2527ef
Alisson Gustavo <gustavoalisson112@gmail.com>
2021-01-05 16:48:06 -0300

    crud com entity

---
+++ CRUD/Models/Contexto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+
+namespace CRUD.Models
+{
+    //Faz a conexão com o banco
+    public class Contexto : DbContext
+    {
+        public DbSet<Pessoa> Pessoas { get; set; }
+    }
+}
---
+++ CRUD/Models/Pessoa.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CRUD.Models
+{
+    public class Pessoa
+    {
+        [Key]
+        public int Codigo { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+    }
+}
